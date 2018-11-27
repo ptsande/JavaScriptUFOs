9aaa6fe76ab6de967dbf63ee85da8524e5e31eee
Patrick Sande <patricktsande@gmail.com>
2018-11-26 22:02:13 -0600

    updating files with progress on js app

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,14 @@
 var tableData = data;
+var submit = d3.select("#filter-btn");
 
+submit.on("click", function(){
+    // prevent refresh of the page
+    d3.event.preventDefault();
+    // select the input element
+    var inputElement = d3.select("#datetime");
+    // get the value property
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    console.log(filteredData);
+});
