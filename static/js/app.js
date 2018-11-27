b31e1b561190e370ca204cdcf031da64c5a10b54
Patrick Sande <patricktsande@gmail.com>
2018-11-27 16:04:15 -0600

    updating files, table now populates with data

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,6 @@
 var tableData = data;
 var submit = d3.select("#filter-btn");
+var tbody = d3.select("tbody");
 
 submit.on("click", function(){
     // prevent refresh of the page
@@ -9,6 +10,30 @@ submit.on("click", function(){
     // get the value property
     var inputValue = inputElement.property("value");
     console.log(inputValue);
-    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    var filteredData = tableData.filter(eventDate => eventDate.datetime === inputValue);
     console.log(filteredData);
 });
+
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
+// // Function to handle input change
+// function handleChange(event) {
+//     // grab the value of the input field
+//     var value = d3.event.target.value;
+  
+//     // clear the existing output
+//     tbody.html("");
+  
+//     var frequencyCounts = counter(value);
+//     Object.entries(frequencyCounts).forEach(([key, value]) => {
+//       var li = tbody.append("li").text(`${key}: ${value}`);
+//     });  
+// }
+  
+// text.on("change", handleChange);
